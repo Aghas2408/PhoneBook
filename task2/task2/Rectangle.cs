4c20c96b1cfc2046b855e58a9af52c4e6eeb5787
unknown <aghasi.seyranyan@gmail.com>
2021-07-23 18:55:02 +0400

    second task not completed

---
+++ task2/task2/Rectangle.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task2.Interfaces;
+
+namespace task2
+{
+    class Rectangle : IRectangle, IMovableRectangle 
+    {
+        public void drawRectangle(int w, int h)
+        {
+            Console.SetCursorPosition(w,h);
+            string s = "╔";
+            string space = "";
+            string first_space = "";
+            for (int i = 0; i < 5; i++)
+            {
+                space += " ";
+                s += "═";
+            }
+
+            for (int i = 0; i <w; i++)
+            {
+  
+                first_space += " ";
+
+            }
+         
+            s += "╗" + "\n";
+
+            for (int i = 0; i < 5; i++)
+                s += first_space + "║" + space + "║" + "\n";
+
+            s +=  first_space + "╚";
+            for (int i = 0; i < 5; i++)
+                s += "═";
+
+            s += "╝" + "\n";
+
+
+            Console.Write(s);
+
+
+        }
+
+        public void moveRectangle()
+        {
+            while (true)
+            {
+                var input = Console.ReadKey();
+                if (input.Key == ConsoleKey.W )
+                {
+                    
+                    if(ConsoleHost.top-1 >= 0)
+                    {
+                        ConsoleHost.top -= 1;
+                        Console.Clear();
+                        drawRectangle(ConsoleHost.left, ConsoleHost.top);
+                    }
+
+                }
+                if (input.Key == ConsoleKey.S)
+                {
+                    if (ConsoleHost.top + 1 <= Console.BufferHeight)
+                    {
+                        ConsoleHost.top += 1;
+                        Console.Clear();
+                        drawRectangle(ConsoleHost.left, ConsoleHost.top);
+                    }
+                }
+                if (input.Key == ConsoleKey.A)
+                {
+                    if (ConsoleHost.left - 1 >= 0)
+                    {
+                        ConsoleHost.left -= 1;
+                        Console.Clear();
+                        drawRectangle(ConsoleHost.left, ConsoleHost.top);
+                    }
+                }
+                if (input.Key == ConsoleKey.D)
+                {
+                    if (ConsoleHost.left + 1 <= Console.BufferWidth)
+                    {
+                        ConsoleHost.left += 1;
+                        Console.Clear();
+                        drawRectangle(ConsoleHost.left, ConsoleHost.top);
+                    }
+                }
+            }
+          
+        }
+    }
+}
